c08ed11ba48907f01e19f8d3d8334b1267aa7f44
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-01 23:39:09 +0200

    Добавьте файлы проекта.

---
+++ Task1.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("13");
+        int number1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("36");
+        int number2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("69");
+        int number3 = int.Parse(Console.ReadLine());
+
+        int maxNumber = Math.Max(Math.Max(number1, number2), number3);
+        int minNumber = Math.Min(Math.Min(number1, number2), number3);
+
+        Console.WriteLine($"Максимальне число: {maxNumber}");
+        Console.WriteLine($"Мінімальне число: {minNumber}");
+    }
+}
